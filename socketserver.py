d67f046826675f5f28586d964286d4d482f1dd18
mueldavc <mueladcm@gmail.com>
2021-02-18 13:42:15 -0300

    18/02/2020

---
+++ socketserver.py
@@ -0,0 +1,62 @@
+import numpy as np
+from datetime import datetime
+from keras.models import load_model
+from os import path
+import time
+import sys
+
+sys.path.append(path.abspath(r'C:\Users\mueld\Documents\Python_Projects\AutoEncoder'))
+from DataSet.DataSet import StockData_robo
+import socket
+
+
+def calcregr(msg=''):
+    regressao = load_model(r'C:\Users\mueld\Documents\Python_Projects\Stock_1\model_linear.h5')
+    autoencoder = load_model(r'C:\Users\mueld\Documents\Python_Projects\Stock_1\model_autoencoder.h5')
+    stockdata = StockData_robo('WIN$', date=datetime.today(), timeframe=15)
+    stockdata.data_final(n_in=17)
+    train_x = stockdata.train_x
+    train_x = train_x.reshape(*train_x.shape, 1)
+    y_pred = autoencoder.predict(train_x)
+    x_train = y_pred.reshape(y_pred.shape[0], -1)
+    y_pred = regressao.predict(x_train)
+    y_pred = np.argmax(y_pred).astype(str)
+    print(y_pred)
+    print('venda' if y_pred == "0" else 'compra')
+    return y_pred
+
+
+class SocketServer:
+    def __init__(self, address='', port=9090):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.address = address
+        self.port = port
+        self.sock.bind((self.address, self.port))
+        self.cummdata = ''
+        self.conn = ''
+        self.addr = ''
+
+    def recvmsg(self):
+        self.sock.listen(1)
+        self.conn, self.addr = self.sock.accept()
+        # print('connected to', self.addr)
+        self.cummdata = ''
+
+        while True:
+            data = self.conn.recv(10000)
+            self.cummdata += data.decode("utf-8")
+            if not data:
+                break
+            self.conn.send(bytes(calcregr(self.cummdata), "utf-8"))
+            return self.cummdata
+
+    def __del__(self):
+        self.sock.close()
+
+
+print('inicio')
+
+serv = SocketServer('127.0.0.1', 9090)
+
+while True:
+    msg = serv.recvmsg()
